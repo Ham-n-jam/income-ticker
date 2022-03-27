a0dc5825bc59882717fe17a8f90dece6b8fd9014
Alex Bell <alexrayquaza@gmail.com>
2022-03-27 13:07:38 +1300

    Fix bug when reloading page on setup screen

--- src/incomeTickerComponent/IncomeTicker.jsx
+++ src/incomeTickerComponent/IncomeTicker.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import IncomeScreen from "./IncomeScreen";
 import SetupScreen from "./setup/SetupScreen";
 
 export default function IncomeTicker() {
   return (
-    <BrowserRouter basename={"/income-ticker"}>
+    <HashRouter basename={"/income-ticker"}>
       <Routes>
         <Route path="/" element={<IncomeScreen />} />
         <Route path="/setup" element={<SetupScreen />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
